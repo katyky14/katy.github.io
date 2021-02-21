24fec02d1d6af2950f12fed9787f55568a5be128
Katy Kam <katykam@Katy-MacBook-Pro.local>
2021-02-21 15:44:10 -0800

    Adding files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+// nav javascript side
+const menuDisplayButton = document.getElementById('menu-trigger');
+
+
+menuDisplayButton.addEventListener("click", displaySideMenuRight);
+
+const menuCloseButton = document.getElementById("menu-close");
+
+
+menuCloseButton.addEventListener("click", hideSideMenuRight);
+
+const sideMenuRight = document.getElementById('side-menu-right');
+console.log(sideMenuRight);
+
+function displaySideMenuRight(){
+console.log ("display was clicked"); sideMenuRight.classList.add("show-menu");
+}
+
+function hideSideMenuRight(){
+  sideMenuRight.classList.remove("show-menu");
+}
+
+
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
